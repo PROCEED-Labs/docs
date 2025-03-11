2061149de36e542ecbabda441f954d80d7ed244f
Kai <iak.tern@gmail.com>
2025-03-11 17:56:43 +0100

    Moved and updated the Script Task API

--- content/developer/_meta.ts
+++ content/developer/_meta.ts
@@ -1,6 +1,7 @@
 import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
+  'script-task-api': 'Script Task API',
   'bpmn': 'BPMN Serialization',
   'ms-api': {
     title: 'MS API',
--- content/developer/bpmn/_meta.ts
+++ content/developer/bpmn/_meta.ts
@@ -1,17 +1,16 @@
-import type { MetaRecord } from "nextra";
+import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
-  "bpmn-supported": "Supported Elements",
-  "bpmn-general-serialization": "General Serialization",
-  "bpmn-data-types": "Process Variables and Types",
-  "bpmn-script-task": "Script Task & API",
-  "bpmn-user-tasks": "User Tasks",
-  "bpmn-subprocesses": "Subprocesses",
-  "bpmn-gateways": "Gateways",
-  "bpmn-error-escalation": "Error & Escalation Events",
-  "bpmn-timer": "Timer Events",
-  "bpmn-constraints": "Process and Task Constraints",
-  "bpmn-capabilities": { display: "hidden" },
+  'bpmn-supported': 'Supported Elements',
+  'bpmn-general-serialization': 'General Serialization',
+  'bpmn-data-types': 'Process Variables and Types',
+  'bpmn-user-tasks': 'User Tasks',
+  'bpmn-subprocesses': 'Subprocesses',
+  'bpmn-gateways': 'Gateways',
+  'bpmn-error-escalation': 'Error & Escalation Events',
+  'bpmn-timer': 'Timer Events',
+  'bpmn-constraints': 'Process and Task Constraints',
+  'bpmn-capabilities': { display: 'hidden' },
 };
 
 export default meta;

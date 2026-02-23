d17ca05a689af6c68484deb455b933bec6945e04
Kai <iak.tern@gmail.com>
2026-02-23 17:40:53 +0100

    Start of User Guide

--- content/user-guide/_meta.ts
+++ content/user-guide/_meta.ts
@@ -1,7 +1,6 @@
 import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
-  'ms-interface-overview': 'Overview',
   'process-list': 'Process List',
   'process-editor': 'Process Editor',
   'automation': { title: 'Process Automation', display: 'normal' },
